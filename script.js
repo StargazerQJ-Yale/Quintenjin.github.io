a10f49aaa15d4a5bc8e7437bfd442d2ee257d67c
Stargazaaa <quinten.jin@yale.edu>
2025-10-25 21:10:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("Welcome to my personal website!");
+console.log("Feel free to explore and learn more about me.");
+console.log("Contact me for collaborations or inquiries.");
+console.log("Thank you for visiting!");
